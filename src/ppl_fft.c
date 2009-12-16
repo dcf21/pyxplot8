bb6bcb38bdfef3e4e1055a6d91fcee011d00632e
dcf21 <dcf21@8e462c0f-ee70-4953-a96e-5db927b55778>
2009-12-16 21:49:39 +0000

    FFT command now working for function inputs
    git-svn-id: svn://127.0.0.1/pplsvn@489 8e462c0f-ee70-4953-a96e-5db927b55778

--- src/ppl_fft.c
+++ src/ppl_fft.c
@@ -48,17 +48,30 @@
 #include "ppl_units_fns.h"
 #include "ppl_userspace.h"
 
+// Window functions for FFTs
+static void fftwindow_rectangular (value *x, int Ndim, int *Npos, int *Nstep) { }
+static void fftwindow_hamming     (value *x, int Ndim, int *Npos, int *Nstep) { double y=1.0; int i; for (i=0; i<Ndim; i++) { y*=0.54-0.46*cos(2*M_PI*((double)Npos[i])/((double)(Nstep[i]-1))); } x->real*=y; x->imag*=y; if (x->imag==0.0) { x->imag=0.0; x->FlagComplex=0; } }
+static void fftwindow_hann        (value *x, int Ndim, int *Npos, int *Nstep) { double y=1.0; int i; for (i=0; i<Ndim; i++) { y*=0.5*(1.0-cos(2*M_PI*((double)Npos[i])/((double)(Nstep[i]-1)))); } x->real*=y; x->imag*=y; if (x->imag==0.0) { x->imag=0.0; x->FlagComplex=0; } }
+static void fftwindow_cosine      (value *x, int Ndim, int *Npos, int *Nstep) { double y=1.0; int i; for (i=0; i<Ndim; i++) { y*=sin(M_PI*((double)Npos[i])/((double)(Nstep[i]-1))); } x->real*=y; x->imag*=y; if (x->imag==0.0) { x->imag=0.0; x->FlagComplex=0; } }
+static void fftwindow_lanczos     (value *x, int Ndim, int *Npos, int *Nstep) { double y=1.0,z; int i; for (i=0; i<Ndim; i++) { z=2*M_PI*((double)Npos[i])/((double)(Nstep[i]-1))-M_PI; y*=sin(z)/z; } x->real*=y; x->imag*=y; if (x->imag==0.0) { x->imag=0.0; x->FlagComplex=0; } }
+static void fftwindow_bartlett    (value *x, int Ndim, int *Npos, int *Nstep) { double y=1.0; int i; for (i=0; i<Ndim; i++) { y*=(2.0/(Nstep[i]-1))*((Nstep[i]-1)/2.0-fabs(Npos[i]-(Nstep[i]-1)/2.0)); } x->real*=y; x->imag*=y; if (x->imag==0.0) { x->imag=0.0; x->FlagComplex=0; } }
+static void fftwindow_triangular  (value *x, int Ndim, int *Npos, int *Nstep) { double y=1.0; int i; for (i=0; i<Ndim; i++) { y*=(2.0/(Nstep[i]  ))*((Nstep[i]  )/2.0-fabs(Npos[i]-(Nstep[i]-1)/2.0)); } x->real*=y; x->imag*=y; if (x->imag==0.0) { x->imag=0.0; x->FlagComplex=0; } }
+static void fftwindow_gauss       (value *x, int Ndim, int *Npos, int *Nstep) { double y=1.0; const double sigma=0.5; int i; for (i=0; i<Ndim; i++) { y*=exp(-0.5*pow((Npos[i]-((Nstep[i]-1)/2.0))/(sigma*(Nstep[i]-1)/2.0),2.0)); } x->real*=y; x->imag*=y; if (x->imag==0.0) { x->imag=0.0; x->FlagComplex=0; } }
+static void fftwindow_bartletthann(value *x, int Ndim, int *Npos, int *Nstep) { double y=1.0; int i; for (i=0; i<Ndim; i++) { y*=0.62 - 0.48*(((double)Npos[i])/((double)(Nstep[i]-1))-0.5) - 0.38*cos(2*M_PI*((double)Npos[i])/((double)(Nstep[i]-1))); } x->real*=y; x->imag*=y; if (x->imag==0.0) { x->imag=0.0; x->FlagComplex=0; } }
+static void fftwindow_blackman    (value *x, int Ndim, int *Npos, int *Nstep) { double y=1.0; const double alpha=0.16; int i; for (i=0; i<Ndim; i++) { y*=(1.0-alpha)/2.0 - 0.5*cos(2*M_PI*((double)Npos[i])/((double)(Nstep[i]-1))) + alpha/2.0*cos(4*M_PI*((double)Npos[i])/((double)(Nstep[i]-1))); } x->real*=y; x->imag*=y; if (x->imag==0.0) { x->imag=0.0; x->FlagComplex=0; } }
+
 // Main entry point for the implementation of the fft command
 int directive_fft(Dict *command)
  {
-  int           i, status, Ndims, Nsamples, Nsteps[USING_ITEMS_MAX];
+  int           i, j, k, l, status, Ndims, Nsamples, Nsteps[USING_ITEMS_MAX], Npos[USING_ITEMS_MAX];
   int           ContextOutput, ContextLocalVec, ContextDataTab;
   FunctionDescriptor *FuncPtr, *FuncPtrNext, *FuncPtr2;
   FFTDescriptor      *output;
-  double        TempDbl;
-  value        *min[USING_ITEMS_MAX], *max[USING_ITEMS_MAX], *step[USING_ITEMS_MAX];
+  double        TempDbl, pos[USING_ITEMS_MAX];
+  value        *min[USING_ITEMS_MAX], *max[USING_ITEMS_MAX], *step[USING_ITEMS_MAX], x, FirstVal;
   unsigned char inverse;
-  char         *cptr, *filename, *outfunc;
+  char         *cptr, *filename, *outfunc, *infunc, *scratchpad, *errtext;
+  void (*WindowType)(value *,int,int *,int *);
   wordexp_t     WordExp;
   glob_t        GlobData;
   List         *RangeList;
@@ -115,6 +128,22 @@ int directive_fft(Dict *command)
   ContextLocalVec= lt_DescendIntoNewContext();
   ContextDataTab = lt_DescendIntoNewContext();
 
+  // Work out what type of window we're going to use
+  WindowType = fftwindow_rectangular;
+  DictLookup(command,"window",NULL,(void **)(&cptr));
+  if (cptr!=NULL)
+   {
+    if      (strcmp(cptr,"hamming"     )!=0) WindowType = fftwindow_hamming;
+    else if (strcmp(cptr,"hann"        )!=0) WindowType = fftwindow_hann;
+    else if (strcmp(cptr,"cosine"      )!=0) WindowType = fftwindow_cosine;
+    else if (strcmp(cptr,"lanczos"     )!=0) WindowType = fftwindow_lanczos;
+    else if (strcmp(cptr,"bartlett"    )!=0) WindowType = fftwindow_bartlett;
+    else if (strcmp(cptr,"triangular"  )!=0) WindowType = fftwindow_triangular;
+    else if (strcmp(cptr,"gauss"       )!=0) WindowType = fftwindow_gauss;
+    else if (strcmp(cptr,"bartletthann")!=0) WindowType = fftwindow_bartletthann;
+    else if (strcmp(cptr,"blackman"    )!=0) WindowType = fftwindow_blackman;
+   }
+
   // Fetch filename of potential datafile to FFT
   DictLookup(command,"filename",NULL,(void **)(&cptr));
 
@@ -139,7 +168,39 @@ int directive_fft(Dict *command)
   // Fetch data which we are going to FFT
   if (cptr != NULL) // We are FFTing data from a file
    {
-   } else { // We are FFTing data from a function
+   }
+  else // We are FFTing data from a function
+   {
+    DictLookup(command, "input_function", NULL, (void **)&infunc);
+    if (infunc == NULL) { ppl_error(ERR_INTERNAL, "ppl_fft could not read name of function for input."); return 1; }
+    DictLookup(_ppl_UserSpace_Funcs, infunc, NULL, (void *)&FuncPtr2);
+    if (FuncPtr2 == NULL) { sprintf(temp_err_string, "No such function as %s()", infunc); ppl_error(ERR_GENERAL, temp_err_string); return 1; }
+    if (FuncPtr2->NumberArguments != Ndims) { sprintf(temp_err_string, "%d-dimensional Fourier transforms can only be performed upon functions which take %d inputs. The supplied function %s() takes %d inputs.", Ndims, Ndims, infunc, FuncPtr2->NumberArguments); ppl_error(ERR_GENERAL, temp_err_string); return 1; }
+
+    scratchpad = (char *)lt_malloc(LSTR_LENGTH);
+    errtext    = (char *)lt_malloc(LSTR_LENGTH);
+    if ((scratchpad == NULL) || (errtext == NULL)) { ppl_error(ERR_MEMORY, "Out of memory"); return 1; }
+
+    status=-1;
+    for (i=0; i<Nsamples; i++)
+     {
+      sprintf(scratchpad,"%s(",infunc);
+      j=strlen(scratchpad);
+      k=i;
+      for (l=Ndims-1; l>=0; l--) { Npos[l] = (k % Nsteps[l]); pos[l] = min[l]->real+step[l]->real*Npos[l]; k /= Nsteps[l]; }
+      for (l=0; l<Ndims; l++) { x=*(min[l]); x.real=pos[l]; sprintf(scratchpad+j,"%s,",ppl_units_NumericDisplay(&x,0,1,20)); j+=strlen(scratchpad+j); }
+      sprintf(scratchpad+j-(Ndims>0),")");
+      ppl_EvaluateAlgebra(scratchpad, &x, 0, NULL, 0, &status, errtext, 0);
+      if ((status>=0)||(!gsl_finite(x.real))||(!gsl_finite(x.imag))) { sprintf(temp_err_string, "Could not evaluate input function at position %s(", infunc); j=strlen(temp_err_string); for (l=0; l<Ndims; l++) { x=*(min[l]); x.real=pos[l]; sprintf(temp_err_string+j,"%s,",ppl_units_NumericDisplay(&x,0,1,0)); j+=strlen(temp_err_string+j); } sprintf(temp_err_string+j-(Ndims>0),")"); ppl_error(ERR_NUMERIC, temp_err_string); return 1; } // Evaluation of algebra failed
+      if (i==0) { FirstVal=x; FirstVal.real=1.0; FirstVal.imag=0.0; FirstVal.FlagComplex=0; }
+      else if (!ppl_units_DimEqual(&x, &FirstVal)) { sprintf(temp_err_string, "The supplied function to FFT does not produce values with consistent units; has produced values with units of <%s> and of <%s>.", ppl_units_GetUnitStr(&FirstVal,NULL,NULL,0,0), ppl_units_GetUnitStr(&x,NULL,NULL,1,0)); ppl_error(ERR_NUMERIC, temp_err_string); return 1; }
+      (*WindowType)(&x, Ndims, Npos, Nsteps); // Apply window function to data
+      #ifdef HAVE_FFTW3
+      datagrid[i][0] = x.real; datagrid[i][1] = x.imag;
+      #else
+      datagrid[i].re = x.real; datagrid[i].im = x.imag;
+      #endif
+     }
    }
 
   // FFT data
@@ -162,6 +223,7 @@ int directive_fft(Dict *command)
   output->invrange = (value *)malloc(Ndims * sizeof(value));
   if ((output->XSize==NULL)||(output->range==NULL)||(output->invrange==NULL)) { free(output); fftw_free(datagrid); ppl_error(ERR_MEMORY,"Out of memory"); return 1; }
   for (i=0; i<Ndims; i++) output->XSize[i] = Nsteps[i];
+  status=0;
   for (i=0; i<Ndims; i++) { ppl_units_sub(max[i], min[i], &output->range[i], &status, temp_err_string); if (status) break; }
   if (status) { ppl_error(ERR_INTERNAL,temp_err_string); free(output); fftw_free(datagrid); free(output->XSize); free(output->range); free(output->invrange); return 1; }
   for (i=0; i<Ndims; i++) { ppl_units_sub(max[i], min[i], &output->range[i], &status, temp_err_string); if (status) break; }
@@ -171,7 +233,7 @@ int directive_fft(Dict *command)
   output->normalisation = 1.0/sqrt((double)Nsamples);
 
   // Make output unit
-  ppl_units_zero(&output->OutputUnit);
+  output->OutputUnit = FirstVal; // Output of an FFT has units of fn being FFTed, multiplied by the reciprocal of the units of all arguments being FFTed
   for (i=0; i<Ndims; i++) { ppl_units_mult(&output->OutputUnit, &output->invrange[i], &output->OutputUnit, &status, temp_err_string); if (status>=0) break; }
   if (status) { ppl_error(ERR_INTERNAL,temp_err_string); free(output); fftw_free(datagrid); free(output->XSize); free(output->range); free(output->invrange); return 1; }
   output->OutputUnit.real = output->OutputUnit.imag = 0.0;
@@ -206,12 +268,16 @@ int directive_fft(Dict *command)
   return 0;
  }
 
+// Function which is called whenever an FFT function is evaluated, to extract value out of data grid
 void ppl_fft_evaluate(char *FuncName, FFTDescriptor *desc, value *in, value *out, int *status, char *errout)
  {
   int i, j;
   double TempDbl;
 
   *out = desc->OutputUnit;
+
+  // Issue user with a warning if complex arithmetic is not enabled
+  if (settings_term_current.ComplexNumbers != SW_ONOFF_ON) ppl_warning(ERR_NUMERIC, "Attempt to evaluate a Fourier transform function whilst complex arithmetic is disabled. Fourier transforms are almost invariably complex and so this is unlikely to work.");
 
   // Check dimensions of input arguments and ensure that they are all real
   for (i=0; i<desc->Ndims; i++)
@@ -237,20 +303,22 @@ void ppl_fft_evaluate(char *FuncName, FFTDescriptor *desc, value *in, value *out
   for (i=0; i<desc->Ndims; i++)
    {
     TempDbl = floor((in+i)->real * desc->range[i].real + 0.5);
-    if ((TempDbl < 0.0) && (TempDbl >= desc->XSize[i])) { return; } // Query out of range; return zero with appropriate output unit
+    if      ((TempDbl >= 0.0) && (TempDbl <= desc->XSize[i]/2)) { } // Positive frequencies stored in lower half of array
+    else if ((TempDbl <  0.0) && (TempDbl >=-desc->XSize[i]/2)) { TempDbl += desc->XSize[i]; } // Negative frequencies stored in upper half of array
+    else                                                        { return; } // Query out of range; return zero with appropriate output unit
     j *= desc->XSize[i];
     j += (int)TempDbl;
    }
 
   // Write output value to out
   #ifdef HAVE_FFTW3
-  out->real = desc->datagrid[j][0];
+  out->real = desc->datagrid[j][0] * desc->normalisation;
   if (desc->datagrid[j][1] == 0.0) { out->FlagComplex = 0; out->imag = 0.0;                  }
-  else                             { out->FlagComplex = 1; out->imag = desc->datagrid[j][1]; }
+  else                             { out->FlagComplex = 1; out->imag = desc->datagrid[j][1] * desc->normalisation; }
   #else
-  out->real = desc->datagrid[j].re;
+  out->real = desc->datagrid[j].re * desc->normalisation;
   if (desc->datagrid[j].im == 0.0) { out->FlagComplex = 0; out->imag = 0.0;                  }
-  else                             { out->FlagComplex = 1; out->imag = desc->datagrid[j].im; }
+  else                             { out->FlagComplex = 1; out->imag = desc->datagrid[j].im * desc->normalisation; }
   #endif
   return;
  }
