08bba238a526b4d5b2d532afdb18ceba9585f90e
dcf21 <dcf21@8e462c0f-ee70-4953-a96e-5db927b55778>
2010-05-15 20:26:07 +0000

    MDS's comments on chapters 1-6 folded into manual
    git-svn-id: svn://127.0.0.1/pplsvn@768 8e462c0f-ee70-4953-a96e-5db927b55778

--- src/ppl_setting_types.c
+++ src/ppl_setting_types.c
@@ -107,7 +107,7 @@ char *SW_DISPLAY_STR[] = {"natural"     , "typeable"     , "latex"        };
 int   SW_DISPLAY_ACL[] = {1             , 1              , 1              };
 int   SW_DISPLAY_INT[] = {SW_DISPLAY_N  , SW_DISPLAY_T   , SW_DISPLAY_L   , -1};
 
-char *SW_CALENDAR_STR[] = {"Gregorian"          , "Julian"          , "British"          , "French"          , "Catholic"          , "Russian"          , "Greek"          , "Hebrew"          , "Islamic"          };
+char *SW_CALENDAR_STR[] = {"Gregorian"          , "Julian"          , "British"          , "French"          , "Papal"             , "Russian"          , "Greek"          , "Hebrew"          , "Islamic"          };
 int   SW_CALENDAR_INT[] = {SW_CALENDAR_GREGORIAN, SW_CALENDAR_JULIAN, SW_CALENDAR_BRITISH, SW_CALENDAR_FRENCH, SW_CALENDAR_CATHOLIC, SW_CALENDAR_RUSSIAN, SW_CALENDAR_GREEK, SW_CALENDAR_HEBREW, SW_CALENDAR_ISLAMIC, -1};
 int   SW_CALENDAR_ACL[] = {1                    , 1                 , 1                  , 1                 , 1                   , 1                  , 5                , 1                 , 1                  , -1};
 
